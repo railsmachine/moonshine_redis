c15994a327db6c0d73c1371983ca8f2d745ceec4
Ches Martin <ches@whiskeyandgrits.net>
2010-01-22 05:17:25 +0700

    Functioning Redis plugin and specs

--- lib/redis.rb
+++ lib/redis.rb
@@ -3,17 +3,43 @@ module Redis
   # Define options for this plugin via the <tt>configure</tt> method
   # in your application manifest:
   #
-  #   configure(:redis => {:foo => true})
+  #   configure(:redis => { :arch => 'i386', :ruby_client => :latest })
   #
   # Then include the plugin and call the recipe(s) you need:
   #
   #  plugin :redis
   #  recipe :redis
-  def redis(options = {})
-    # define the recipe
-    # options specified with the configure method will be 
-    # automatically available here in the options hash.
-    #    options[:foo]   # => true
+  def redis(options={})
+    options = { :enable_on_boot => true }.merge(options)
+    arch = options[:arch] || 'amd64'
+
+    package 'wget', :ensure => :installed
+    exec 'download redis',
+      :command => "wget http://http.us.debian.org/debian/pool/main/r/redis/redis-server_1.2.0-1_#{arch}.deb",
+      :require => package('wget'),
+      :cwd     => '/usr/local/src',
+      :creates => "/usr/local/src/redis-server_1.2.0-1_#{arch}.deb"
+    package 'redis-server',
+      :ensure   => :installed,
+      :provider => :dpkg,
+      :source   => "/usr/local/src/redis-server_1.2.0-1_#{arch}.deb",
+      :require  => exec('download redis')
+
+    service 'redis-server',
+      :ensure  => :running,
+      :enable  => options[:enable_on_boot],
+      :require => package('redis-server')
+    file '/etc/redis', :ensure => :directory
+    file '/etc/redis/redis.conf',
+      :ensure  => :present,
+      :mode    => '644',
+      :notify  => service('redis-server'),
+      :content => template(File.join(File.dirname(__FILE__), '..', 'templates', 'redis.conf.erb'), binding)
+
+    # install client gem if specified
+    if options[:ruby_client]
+      gem 'redis', :require => package('redis-server'), :version => options[:ruby_client]
+    end
   end
-  
+
 end
--- spec/redis_spec.rb
+++ spec/redis_spec.rb
@@ -1,24 +1,48 @@
 require File.join(File.dirname(__FILE__), 'spec_helper.rb')
 
-class RedisManifest < Moonshine::Manifest
+class RedisManifest < Moonshine::Manifest::Rails
   plugin :redis
 end
 
 describe "A manifest with the Redis plugin" do
-  
+
   before do
     @manifest = RedisManifest.new
     @manifest.redis
   end
-  
-  it "should be executable" do
-    @manifest.should be_executable
+
+  it "should download the redis package for the specified arch, defaulting to amd64" do
+    @manifest.execs['download redis'].command.should match(/amd64/)
+    @manifest.redis(:arch => 'i386')
+    @manifest.execs['download redis'].command.should match(/i386/)
   end
-  
-  #it "should provide packages/services/files" do
-  # @manifest.packages.keys.should include 'foo'
-  # @manifest.files['/etc/foo.conf'].content.should match /foo=true/
-  # @manifest.execs['newaliases'].refreshonly.should be_true
-  #end
-  
+
+  it "should install the redis-server package" do
+    @manifest.packages.keys.should include('redis-server')
+  end
+
+  it "should configure and run the redis-server service" do
+    @manifest.services.keys.should include('redis-server')
+    @manifest.services['redis-server'].ensure.should == :running
+  end
+
+  it "should allow running service at boot to be disabled" do
+    @manifest.services['redis-server'].enable.should == true
+    @manifest.redis(:enable_on_boot => false)
+    @manifest.services['redis-server'].enable.should_not == true
+  end
+
+  it "should install the redis configuration file" do
+    @manifest.files['/etc/redis/redis.conf'].should_not be(nil)
+  end
+  it "should install the Ruby client library for Redis iff the option is given" do
+    @manifest.packages.keys.should_not include('redis')
+
+    # stub so Gem ignores what you really have installed locally
+    Gem.stub_chain(:source_index, :search => [])
+    @manifest.redis(:ruby_client => '0.1.1')
+    @manifest.packages.keys.should include('redis')
+    @manifest.packages['redis'].ensure.should == '0.1.1'
+  end
+
 end
