d7d2c683693d0ead7fbb814bae4e093e05578960
Jesse Newland <jesse@railsmachine.com>
2011-02-23 08:03:22 -0500

    compile from source, 2.2.0 as the default

--- lib/moonshine/redis.rb
+++ lib/moonshine/redis.rb
@@ -11,25 +11,50 @@ module Moonshine
     #  recipe :redis
     def redis(options={})
       options = HashWithIndifferentAccess.new({ :enable_on_boot => true }.merge(options))
-      arch = options[:arch] || Facter.architecture == 'i386' ? 'i386' : 'amd64' # FIXME, could be smarter, but, at least it's configurable
-      version = options[:version] || '2.0.1-2'
+      make_command = options[:arch] || Facter.architecture == 'i386' ? 'make 32bit' : 'make'
+      version = options[:version] || '2.2.0'
 
       package 'wget', :ensure => :installed
       exec 'download redis',
-        :command => "wget http://http.us.debian.org/debian/pool/main/r/redis/redis-server_#{version}_#{arch}.deb",
+        :command => "wget http://redis.googlecode.com/files/redis-#{options[:version]}.tar.gz",
         :require => package('wget'),
         :cwd     => '/usr/local/src',
-        :creates => "/usr/local/src/redis-server_#{version}_#{arch}.deb"
+        :creates => "/usr/local/src/redis-#{options[:version]}.tar.gz"
+      exec 'untar redis',
+        :command => "tar xzvf redis-#{options[:version]}.tar.gz",
+        :require => exec('download redis'),
+        :cwd     => '/usr/local/src',
+        :creates => "/usr/local/src/redis-#{options[:version]}"
+      exec 'compile redis',
+        :command => make_command,
+        :require => exec('untar redis'),
+        :cwd     => "/usr/local/src/redis-#{options[:version]}",
+        :creates => "/usr/local/src/redis-#{options[:version]}/redis-server"
+      exec 'shutdown redis',
+        :command => "redis-cli shutdown || true",
+        :timeout => 0,
+        :require => exec('compile redis'),
+        :refreshonly => true
       package 'redis-server',
-        :ensure   => :installed,
+        :ensure   => :absent,
         :provider => :dpkg,
-        :source   => "/usr/local/src/redis-server_#{version}_#{arch}.deb",
-        :require  => exec('download redis')
+        :require   => exec('shutdown redis')
+      exec 'install redis',
+        :command => "make install",
+        :require => package('redis-server'),
+        :cwd     => "/usr/local/src/redis-#{options[:version]}",
+        :unless => "/usr/local/bin/redis-server --version | grep 'Redis server version #{options[:version]}$'"
 
       service 'redis-server',
         :ensure  => :running,
         :enable  => options[:enable_on_boot],
-        :require => package('redis-server')
+        :require => [package('redis-server'), exec('install redis'), file('/etc/init.d/redis-server')]
+
+      file '/etc/init.d/redis-server',
+        :ensure  => :present,
+        :mode    => '755',
+        :content => template(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'init.d'), binding)
+
       file '/etc/redis', :ensure => :directory
       file '/etc/redis/redis.conf',
         :ensure  => :present,
